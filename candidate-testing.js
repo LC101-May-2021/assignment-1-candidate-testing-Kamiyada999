ce579b44b6dd16ed1cd1a49063d733356249ec9e
Tyler Holt <84360012+Kamiyada999@users.noreply.github.com>
2021-06-09 10:30:57 +0000

    Candidate name + period

--- candidate-testing.js
+++ candidate-testing.js
@@ -21,7 +21,7 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  let array = ["Who was the first American woman in space?" , "True or false: 5 kilometer == 5000 meters?" , "(5 + 3)/2 * 10 = ?" , "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?" , "What is the minimum crew size for the ISS?"];
+  let array = ["Who was the first American woman in space?" , "True or false: 5 kilometer == 5000 meters." , "(5 + 3)/2 * 10 = ?" , "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?" , "What is the minimum crew size for the ISS?"];
  
  let array1 = ["Sally Ride" , "true" , "40" , "Trajectory" , "3"];
  
@@ -65,7 +65,7 @@ let array = ["Who was the first American woman in space?" , "True or false: 5 ki
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  console.log("Hello, " + candidateName + "!");
+  console.log("Candidate Name: " + candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
