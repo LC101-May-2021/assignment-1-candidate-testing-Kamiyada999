e6edf2ad38ba0f54244078af50daa86ab69edc35
Tyler Holt <84360012+Kamiyada999@users.noreply.github.com>
2021-06-10 01:59:41 +0000

    added logic to check multiple answers of varying formats

--- candidate-testing.js
+++ candidate-testing.js
@@ -24,6 +24,7 @@ function askQuestion() {
   let array = ["Who was the first American woman in space? " , "True or false: 5000 meters = 5 kilometers. " , "(5 + 3)/2 * 10 = ? " , "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? " , "What is the minimum crew size for the ISS? "];
  
  let array1 = ["sally ride" , "true" , "40" , "trajectory" , "3"];
+ let array2 = ["Sally Ride" , "True" , "40" , "Trajectory" , "3"];
  
  
 }
@@ -31,6 +32,7 @@ function askQuestion() {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 function gradeQuiz(candidateAnswers) {
+
 let numberOfCorrectAnswers = 0;
 let array = ["Who was the first American woman in space? " , "True or false: 5000 meters = 5 kilometers. " , "(5 + 3)/2 * 10 = ? " , "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? " , "What is the minimum crew size for the ISS? "];
  let answer = [];
@@ -38,7 +40,7 @@ let array = ["Who was the first American woman in space? " , "True or false: 500
  let array2 = ["Sally Ride" , "True" , "40" , "Trajectory" , "3"];
  for (let i = 0; i < array1.length; i++) {
  answer.push(input.question(array[i]));
- if (answer[i] === array1[i]) {
+ if (answer[i] === array1[i] || answer[i] === array2[i]) {
    numberOfCorrectAnswers++;
    console.log(`Your Answer: ${array1[i].toLowerCase()}`);
    
@@ -65,7 +67,6 @@ let array = ["Who was the first American woman in space? " , "True or false: 500
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  console.log(candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
