02e4caecb4873c0835d1a838eefdc419883460b7
Tyler Holt <84360012+Kamiyada999@users.noreply.github.com>
2021-06-10 03:03:49 +0000

    Working code with fixes

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,12 +5,12 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let question = "1) Who was the first American woman in space?" ;
+let correctAnswer = ["sally ride" , "true" , 40 , "trajectory" , 3];
+let candidateAnswer = ' ';
+let questions =  ["1) Who was the first American woman in space? " , "2) True or false: 5000 meters = 5 kilometers. " , "3) (5 + 3)/2 * 10 = ? " , "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? " , "5) What is the minimum crew size for the ISS? "];
+let correctAnswers = ["sally ride" , "true" , 40 , "trajectory" , 3];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -21,39 +21,45 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  let array = ["1) Who was the first American woman in space? " , "2) True or false: 5000 meters = 5 kilometers. " , "3) (5 + 3)/2 * 10 = ? " , "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? " , "5) What is the minimum crew size for the ISS? "];
+  //let array = ["1) Who was the first American woman in space? " , "2) True or false: 5000 meters = 5 kilometers. " , "3) (5 + 3)/2 * 10 = ? " , "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? " , "5) What is the minimum crew size for the ISS? "];
  
- let array1 = ["sally ride" , "true" , 40 , "trajectory" , 3];
- let array2 = ["Sally Ride" , "True" , 40 , "Trajectory" , 3];
+// let array1 = ["sally ride" , "true" , 40 , "trajectory" , 3];
+ //let array2 = ["Sally Ride" , "True" , 40 , "Trajectory" , 3];
  
- 
+for (let i = 0; i < questions.length; i ++){
+ candidateAnswers[i] = input.question(console.log(questions[i])); 
+ console.log(`Your Answer:  ${candidateAnswers[i]}
+Correct Answer: ${correctAnswers[i]}`)
 }
-
-
+}
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 function gradeQuiz(candidateAnswers) {
 
 let numberOfCorrectAnswers = 0;
-let array = ["1) Who was the first American woman in space? " , "2) True or false: 5000 meters = 5 kilometers. " , "3) (5 + 3)/2 * 10 = ? " , "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? " , "5) What is the minimum crew size for the ISS? "];
- let answer = [];
- let array1 = ["sally ride" , "true" , "40" , "trajectory" , "3"];
- let array2 = ["Sally Ride" , "True" , "40" , "Trajectory" , "3"];
- for (let i = 0; i < array1.length; i++) {
- answer.push(input.question(array[i]));
- if (answer[i] === array1[i] || answer[i] === array2[i]) {
-   numberOfCorrectAnswers++;
-   console.log(`Your Answer: ${array1[i].toLowerCase()}`);
-   
- } else console.log(`Your Answer: ${answer[i].toLowerCase()}`);
-      
- console.log(`Correct Answer: ${array1[i].toLowerCase()}`);
- }
+//let array = ["1) Who was the first American woman in space? " , "2) True or false: 5000 meters = 5 kilometers. " , "3) (5 + 3)/2 * 10 = ? " , "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? " , "5) What is the minimum crew size for the ISS? "];
+ //let answer = [];
+ //let array1 = ["sally ride" , "true" , "40" , "trajectory" , "3"];
+ //let array2 = ["Sally Ride" , "True" , "40" , "Trajectory" , "3"];
+//for (let i = 0; i < questions.length; i ++){
+ //array1[i] = input.array(console.log(array[i]));
+
+//console.log(`Your Answer:  ${answer[i]}
+//Correct Answer: ${array1[i]}`)
 
 
 
+  let grade = 0; 
+for (let i=0; i < 5; i++){
+    if (candidateAnswers[i].toLowerCase() == candidateAnswers[i].toLowerCase()){
+    
+    
+      numberOfCorrectAnswers++
+    }
+  }
+grade = (numberOfCorrectAnswers/questions.length) * 100;
 
-  let grade = (numberOfCorrectAnswers) / (array.length) * 100;
-  console.log(`>>> Overall Grade: ${grade}% (${numberOfCorrectAnswers} of ${array.length} are correct) <<<`);
+
+  console.log(`>>> Overall Grade: ${grade}% (${numberOfCorrectAnswers} of ${questions.length} are correct) <<<`);
   if (grade >= 60) {
     console.log(">>> Status: PASSED <<<");
   } else {
@@ -67,6 +73,7 @@ let array = ["1) Who was the first American woman in space? " , "2) True or fals
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
+  console.log("Candidate Name:  " + candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
@@ -84,3 +91,5 @@ module.exports = {
   gradeQuiz: gradeQuiz,
   runProgram: runProgram
 }
+
+
