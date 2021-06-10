824980ded0c8dad5241f35e7d0114e4ba2e48da4
Tyler Holt <84360012+Kamiyada999@users.noreply.github.com>
2021-06-10 01:28:56 +0000

    Corrected spacing

--- candidate-testing.js
+++ candidate-testing.js
@@ -21,7 +21,7 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  let array = ["Who was the first American woman in space?" , "True or false: 5000 meters = 5 kilometers." , "(5 + 3)/2 * 10 = ?" , "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?" , "What is the minimum crew size for the ISS?"];
+  let array = ["Who was the first American woman in space? " , "True or false: 5000 meters = 5 kilometers. " , "(5 + 3)/2 * 10 = ? " , "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? " , "What is the minimum crew size for the ISS? "];
  
  let array1 = ["sally ride" , "true" , "40" , "trajectory" , "3"];
  
@@ -32,7 +32,7 @@ function askQuestion() {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 function gradeQuiz(candidateAnswers) {
 let numberOfCorrectAnswers = 0;
-let array = ["Who was the first American woman in space?" , "True or false: 5000 meters = 5 kilometers." , "(5 + 3)/2 * 10 = ?" , "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?" , "What is the minimum crew size for the ISS?"];
+let array = ["Who was the first American woman in space? " , "True or false: 5000 meters = 5 kilometers. " , "(5 + 3)/2 * 10 = ? " , "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? " , "What is the minimum crew size for the ISS? "];
  let answer = [];
  let array1 = ["sally ride" , "true" , "40" , "trajectory" , "3"];
  let array2 = ["Sally Ride" , "True" , "40" , "Trajectory" , "3"];
